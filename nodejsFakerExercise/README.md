1e482c84ac35ec39140695389ccc74f1511204e0
Julian Lopez <jlopezsa@gmail.com>
2022-02-27 23:14:41 -0500

    Use of nodejs faker package to generate 1000 aleatory names

--- nodejsFakerExercise/README.md
+++ nodejsFakerExercise/README.md
@@ -1,1 +1,24 @@
+# Introduction
+This exercise creates 1000 aleatory names using the [<code>faker</code>](https://www.npmjs.com/package/@faker-js/faker) package.
 
+The 1000 names are writing in the 1000Names.txt file.
+
+Some code part, to generate the 1000 names, is:
+
+```js
+for (let i = 0; i < namesMax; i++) {
+    let randomName = faker.name.findName(); // Rowan Nikolaus
+    fs.appendFile("1000Names.txt",`${i}) ${randomName}\n`,err => {
+        if (err) {
+            console.log(err)
+        }
+    });
+}
+```
+
+The result is shown in the following figure:
+
+<img src="./figures/1000_names_errors.jpg" width="300"/>
+
+However, the number sequence is shown in disorder. 
+**This issue needs to be fixed.**
--- nodejsFakerExercise/fakerNames.js
+++ nodejsFakerExercise/fakerNames.js
@@ -1,2 +1,29 @@
+/**Program to generate 1000 aleatory names
+ * 
+ */
+
+const namesMax = 1000;
+console.log(`Program to generate ${namesMax} aletory names`)
 const { faker } = require('@faker-js/faker');
-const randomName = faker.name.findName(); // Rowan Nikolaus
+const fs = require('fs')
+
+
+var first_line = "This file contains 1000 aleatory names:\n";
+fs.writeFile("1000Names.txt",first_line,err => {
+    if (err) {
+        console.log(err)
+    }else{
+        console.log("The file was create successfully")
+    }
+});
+
+for (let i = 0; i < namesMax; i++) {
+    let randomName = faker.name.findName(); // Rowan Nikolaus
+    fs.appendFile("1000Names.txt",`${i+1}) ${randomName}\n`,err => {
+        if (err) {
+            console.log(err)
+        }
+    });
+}
+
+console.log(`\n...Finished`)
